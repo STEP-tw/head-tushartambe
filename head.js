bf75dca8ffa4bf07c8c54b4d5c7403e4050d312c
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2018-12-06 10:40:11 +0530

    Implemented readFileContents() to read files.

--- head.js
+++ head.js
@@ -13,8 +13,18 @@
   node ./head.js -c5 file1 file2
   node ./head.js -c 5 file1 file2
 */
+const fs = require('fs');
 
 const {
   separateCmdLineArgs } = require('./src/lib.js');
 
+const readFileContents = function(fileName) {
+  return fs.readFileSync(fileName,'utf8');
+}
 
+const readFilesAndInputs = function(cmdArgs) {
+  cmdLineInputs = seperateCmdArgs(cmdArgs);
+  cmdLineInputs.fileContents = cmdLineInputs['files'].map(readFileContents);
+
+  return cmdLineInputs;
+}
