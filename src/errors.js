a4dfa68d36263bda4fc2cc8a1551629ef9505ea7
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2018-12-11 12:25:20 +0530

    Error handling conditions extracted into errors.js

---
+++ src/errors.js
@@ -0,0 +1,54 @@
+const  { isOption,
+  isCountOption,
+  isCharacterOption,
+  isOptionZero,
+  isZero,
+  isInvalidOption,
+  isInvalidCount } = require('./util.js');
+
+const errors = {
+  head : function(headData,organizedData,isFileExists) {
+    let { count, files, option} = organizedData;
+
+    if( isZero(headData[0]) || isZero(count) ){
+      return 'head: illegal line count -- 0'
+    }
+
+    if(isInvalidOption(headData[0])){
+      let errorMsg = 'head: illegal option -- '+headData[0][1]+'\nusage: head [-n lines | -c bytes] [file ...]';
+      return errorMsg;
+    }
+
+    if(isInvalidCount(count)) {
+      return (option == 'n') ? 'head: illegal line count -- '+headData[0].slice(2) : 'head: illegal byte count -- '+headData[0].slice(2);
+    }
+
+    if(files.length == 1 && !isFileExists(files[0])){
+      return 'head: '+files[0]+': No such file or directory'
+    }
+  },
+  tail : function(headData,organizedData,isFileExists) {
+    let { count, files, option} = organizedData;
+
+    if( isZero(headData[0]) || isZero(count) ){
+      return '';
+    }
+
+    if(isInvalidOption(headData[0])){
+      let errorMsg = 'tail: illegal option -- '+headData[0][1]+'\nusage: tail [-F | -f | -r] [-q] [-b # | -c # | -n #] [file ...]';
+      return errorMsg;
+    }
+
+    if(isInvalidCount(count)) {
+      return 'tail: illegal offset -- '+headData[0].slice(2);
+    }
+
+    if(files.length == 1 && !isFileExists(files[0])){
+      return 'tail: '+files[0]+': No such file or directory'
+    }
+  }
+}
+
+module.exports = {
+  errors
+};
--- src/lib.js
+++ src/lib.js
@@ -7,6 +7,8 @@ const  {
   isInvalidOption,
   isInvalidCount } = require('./util.js');
 
+const { errors } = require('./errors.js');
+
 const separateCmdLineArgs = function(cmdArgs) {
   let cmdLineInputs = {
     option: 'n',
@@ -71,49 +73,6 @@ const readFile = function(organizedData,fileReader,isFileExists) {
   }
 
   return formatedData.join("\n");
-}
-
-const errors = {
-  head : function(headData,organizedData,isFileExists) {
-    let { count, files, option} = organizedData;
-
-    if( isZero(headData[0]) || isZero(count) ){
-      return 'head: illegal line count -- 0'
-    }
-
-    if(isInvalidOption(headData[0])){
-      let errorMsg = 'head: illegal option -- '+headData[0][1]+'\nusage: head [-n lines | -c bytes] [file ...]';
-      return errorMsg;
-    }
-
-    if(isInvalidCount(count)) {
-      return (option == 'n') ? 'head: illegal line count -- '+headData[0].slice(2) : 'head: illegal byte count -- '+headData[0].slice(2);
-    }
-
-    if(files.length == 1 && !isFileExists(files[0])){
-      return 'head: '+files[0]+': No such file or directory'
-    }
-  },
-  tail : function(headData,organizedData,isFileExists) {
-    let { count, files, option} = organizedData;
-
-    if( isZero(headData[0]) || isZero(count) ){
-      return '';
-    }
-
-    if(isInvalidOption(headData[0])){
-      let errorMsg = 'tail: illegal option -- '+headData[0][1]+'\nusage: tail [-F | -f | -r] [-q] [-b # | -c # | -n #] [file ...]';
-      return errorMsg;
-    }
-
-    if(isInvalidCount(count)) {
-      return 'tail: illegal offset -- '+headData[0].slice(2);
-    }
-
-    if(files.length == 1 && !isFileExists(files[0])){
-      return 'tail: '+files[0]+': No such file or directory'
-    }
-  }
 }
 
 const readFilesAndErrorHandler = function(headData,organizedData,fileReader,isFileExists,funName) {
