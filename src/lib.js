13d1a8d058147d895401aa375f0dee91247c8e6e
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2018-12-11 15:14:05 +0530

    Organizing command line inputs function is extracted into parser.js file.

--- src/lib.js
+++ src/lib.js
@@ -8,41 +8,7 @@ const  {
   isInvalidCount } = require('./util.js');
 
 const { errors } = require('./errors.js');
-
-const separateCmdLineArgs = function(cmdArgs) {
-  let cmdLineInputs = {
-    option: 'n',
-    count: 10,
-    files: []
-  }
-
-  let options = cmdArgs[0].split("");
-
-  if(isCountOption(options)) {
-    +options[1] && (cmdLineInputs.option = 'n' , cmdLineInputs.count = +options.slice(1).join(""));
-    +options[1] || (cmdLineInputs.option = options[1] , cmdLineInputs.count = +options.slice(2).join("")) ;
-
-    cmdLineInputs.files = cmdArgs.slice(1);
-  }
-
-  if(isCharacterOption(options)) {
-    +options[1] || (cmdLineInputs.option = options[1],cmdLineInputs.count = cmdArgs[1],cmdLineInputs.files = cmdArgs.slice(2));
-    +options[1] && (cmdLineInputs.option = 'n', cmdLineInputs.count =options[1], cmdLineInputs.files = cmdArgs.slice(1));
-  }
-
-  if(isOptionZero(options)) {
-    cmdLineInputs.option = 'n';
-    cmdLineInputs.count = 0;
-    cmdLineInputs.files = cmdArgs.slice(1);
-  }
-
-  if(!options.includes('-')){
-    cmdLineInputs.files = cmdArgs.slice(0);
-    cmdLineInputs.option = 'n';
-    cmdLineInputs.count = 10;
-  }
-  return cmdLineInputs;
-}
+const { separateCmdLineArgs } = require('./parser.js');
 
 const getBytes = function(count,contents) {
   return contents.split("").slice(0,count).join("");
---
+++ src/parser.js
@@ -0,0 +1,47 @@
+const  {
+  isOption,
+  isCountOption,
+  isCharacterOption,
+  isOptionZero,
+  isZero,
+  isInvalidOption,
+  isInvalidCount } = require('./util.js');
+
+const separateCmdLineArgs = function(cmdArgs) {
+  let cmdLineInputs = {
+    option: 'n',
+    count: 10,
+    files: []
+  }
+
+  let options = cmdArgs[0].split("");
+
+  if(isCountOption(options)) {
+    +options[1] && (cmdLineInputs.option = 'n' , cmdLineInputs.count = +options.slice(1).join(""));
+    +options[1] || (cmdLineInputs.option = options[1] , cmdLineInputs.count = +options.slice(2).join("")) ;
+
+    cmdLineInputs.files = cmdArgs.slice(1);
+  }
+
+  if(isCharacterOption(options)) {
+    +options[1] || (cmdLineInputs.option = options[1],cmdLineInputs.count = cmdArgs[1],cmdLineInputs.files = cmdArgs.slice(2));
+    +options[1] && (cmdLineInputs.option = 'n', cmdLineInputs.count =options[1], cmdLineInputs.files = cmdArgs.slice(1));
+  }
+
+  if(isOptionZero(options)) {
+    cmdLineInputs.option = 'n';
+    cmdLineInputs.count = 0;
+    cmdLineInputs.files = cmdArgs.slice(1);
+  }
+
+  if(!options.includes('-')){
+    cmdLineInputs.files = cmdArgs.slice(0);
+    cmdLineInputs.option = 'n';
+    cmdLineInputs.count = 10;
+  }
+  return cmdLineInputs;
+}
+
+module.exports = {
+  separateCmdLineArgs
+};
