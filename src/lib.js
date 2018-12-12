648ac1ef8bab64d886cfcf3db2d5f4eafc5e9c41
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2018-12-12 10:12:01 +0530

    Code indentation fixed.

--- src/lib.js
+++ src/lib.js
@@ -1,75 +1,75 @@
 const { errors } = require('./errors.js');
 const { separateCmdLineArgs } = require('./parser.js');
 
-const getBytes = function(count,contents) {
-  return contents.split("").slice(0,count).join("");
+const getBytes = function (count, contents) {
+  return contents.split("").slice(0, count).join("");
 }
 
-const getLines = function(count,contents) {
-  return contents.split("\n").slice(0,count).join("\n");
+const getLines = function (count, contents) {
+  return contents.split("\n").slice(0, count).join("\n");
 }
 
-const readFileContents = function(fileReader,fileName,organizedData) {
-  const {count , readerSelector } = organizedData;
-  return readerSelector(count, fileReader(fileName,'utf8'))
+const readFileContents = function (fileReader, fileName, organizedData) {
+  const { count, readerSelector } = organizedData;
+  return readerSelector(count, fileReader(fileName, 'utf8'))
 }
 
-const readFile = function(organizedData,fileReader,isFileExists,funName) {
+const readFile = function (organizedData, fileReader, isFileExists, funName) {
   let { option, count, files, readerSelector } = organizedData;
-  let formatedData =[];
+  let formatedData = [];
   let delimeter = "";
 
-  for(let counter = 0; counter < files.length; counter++){
-    if(isFileExists(files[counter])) {
-      formatedData.push(delimeter+'==> '+files[counter]+' <==');
-      formatedData.push(readFileContents(fileReader,files[counter],organizedData)); 
+  for (let counter = 0; counter < files.length; counter++) {
+    if (isFileExists(files[counter])) {
+      formatedData.push(delimeter + '==> ' + files[counter] + ' <==');
+      formatedData.push(readFileContents(fileReader, files[counter], organizedData));
       delimeter = "\n";
     } else {
-      formatedData.push(funName+': '+files[counter]+': No such file or directory');
+      formatedData.push(funName + ': ' + files[counter] + ': No such file or directory');
     }
   }
 
   return formatedData.join("\n");
 }
 
-const readFilesAndErrorHandler = function(headData,organizedData,fileReader,isFileExists,funName) {
+const readFilesAndErrorHandler = function (headData, organizedData, fileReader, isFileExists, funName) {
   let { option, count, files, readerSelector } = organizedData;
 
-  let errorMsg = errors[funName](headData,organizedData,isFileExists);
+  let errorMsg = errors[funName](headData, organizedData, isFileExists);
 
-  if(errorMsg || errorMsg == "") {
-    return  errors[funName](headData,organizedData,isFileExists)
+  if (errorMsg || errorMsg == "") {
+    return errors[funName](headData, organizedData, isFileExists)
   }
 
-  if(files.length == 1 ) {
-    return readFileContents(fileReader,files[0],organizedData);
+  if (files.length == 1) {
+    return readFileContents(fileReader, files[0], organizedData);
   }
 
-  return readFile(organizedData,fileReader,isFileExists,funName);
+  return readFile(organizedData, fileReader, isFileExists, funName);
 }
 
-const head = function(headData,fileReader,isFileExists) {
+const head = function (headData, fileReader, isFileExists) {
   let organizedData = separateCmdLineArgs(headData);
-  let readerSelector = { 'c':getBytes,'n':getLines }
+  let readerSelector = { 'c': getBytes, 'n': getLines }
   organizedData['readerSelector'] = readerSelector[organizedData['option']];
 
-  return readFilesAndErrorHandler(headData,organizedData,fileReader,isFileExists,'head');
+  return readFilesAndErrorHandler(headData, organizedData, fileReader, isFileExists, 'head');
 }
 
-const getTailBytes = function(count,contents) {
+const getTailBytes = function (count, contents) {
   return contents.split("").slice(-count).join("");
 }
 
-const getTailLines = function(count,contents) {
+const getTailLines = function (count, contents) {
   return contents.split("\n").slice(-count).join("\n");
 }
 
-const tail = function(tailData,fileReader,isFileExists) {
+const tail = function (tailData, fileReader, isFileExists) {
   let organizedData = separateCmdLineArgs(tailData);
-  let readerSelector = { 'c':getTailBytes,'n':getTailLines }
+  let readerSelector = { 'c': getTailBytes, 'n': getTailLines }
   organizedData['readerSelector'] = readerSelector[organizedData['option']];
 
-  return readFilesAndErrorHandler(tailData,organizedData,fileReader,isFileExists,'tail');
+  return readFilesAndErrorHandler(tailData, organizedData, fileReader, isFileExists, 'tail');
 }
 
 module.exports = {
@@ -79,4 +79,5 @@ module.exports = {
   readFile,
   readFileContents,
   tail,
-  head };
+  head
+};
