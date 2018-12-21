127bf0401c2eee8449f392b2099531966f370912
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2018-12-21 10:41:12 +0530

    modified parseInputs() variable names.

--- src/parser.js
+++ src/parser.js
@@ -23,21 +23,31 @@ const createObject = function (option, count, files) {
 
 const parseInputs = function (cmdArgs) {
   let cmdLineInputs = {};
+  let option = cmdArgs[0][1];
+  let files = cmdArgs.slice(1);
 
   if (isNumberOption(cmdArgs[0])) {
-    cmdLineInputs = createObject('n', Math.abs(cmdArgs[0]), cmdArgs.slice(1));
+    option = 'n';
+    count = Math.abs(cmdArgs[0]);
+    cmdLineInputs = createObject(option, count, files);
   }
 
   if (isCharacterOption(cmdArgs[0])) {
-    cmdLineInputs = createObject(cmdArgs[0][1], cmdArgs[1], cmdArgs.slice(2));
+    count = cmdArgs[1];
+    files = cmdArgs.slice(2);
+    cmdLineInputs = createObject(option, count, files);
   }
 
   if (isOptionWithCount(cmdArgs[0])) {
-    cmdLineInputs = createObject(cmdArgs[0][1], cmdArgs[0].slice(2), cmdArgs.slice(1));
+    count = cmdArgs[0].slice(2);
+    cmdLineInputs = createObject(option, count, files);
   }
 
   if (!isStartsWithHypen(cmdArgs[0])) {
-    cmdLineInputs = createObject('n', 10, cmdArgs.slice(0));
+    option = 'n';
+    count = 10;
+    files = cmdArgs.slice(0);
+    cmdLineInputs = createObject(option, count, files);
   }
 
   return cmdLineInputs;
